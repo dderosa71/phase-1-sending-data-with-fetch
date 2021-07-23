c371255bee6e0e14c194fa292288670a9388906c
Dominic DeRosa <dderosa71@gmail.com>
2021-07-23 14:19:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 // Add your code here
 function submitData(userName, userEmail){
+
     const formData = {userName: `${userName}`, userEmail: `${userEmail}`};
 
     const configurationObject = {
         method: 'POST',
-        headers: {'Content-Type' : 'application/json'},
+        headers: {'Content-Type' : 'application/json',
+                 "Accept": "application/json"},
         body: JSON.stringify(formData)}
 
     fetch('http://localhost:3000/users', configurationObject)
-        
-//     .then(resp => resp.JSON())
-// //     .then(resp => {
-// //         // let body = document.querySelector('body')
-// //         let div = body.createElement("div");
-// //         div.append(`${resp}`)
-// //     })
+    .then(resp => resp.JSON())
+    .then(resp => {
+        // let body = document.querySelector('body')
+        let div = body.createElement("div");
+        div.append(`${resp}`)
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    })
 }
+
+    
+    // .then(resp => resp.JSON())
+    // .then(resp => {
+    //     // let body = document.querySelector('body')
+    //     let div = body.createElement("div");
+    //     div.append(`${resp}`)
+    // })
+
