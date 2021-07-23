63d5ba3a453b7b5129d2c712f5c222d42d20b061
Dominic DeRosa <dderosa71@gmail.com>
2021-07-23 12:41:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const formData = {userName: `${userName}`, userEmail: `${userEmail}`};
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(formData)}
+
+    fetch('http://localhost:3000/users', configurationObject)
+        
+//     .then(resp => resp.JSON())
+// //     .then(resp => {
+// //         // let body = document.querySelector('body')
+// //         let div = body.createElement("div");
+// //         div.append(`${resp}`)
+// //     })
+}
